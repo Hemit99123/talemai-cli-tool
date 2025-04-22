5b34ed09bcfbb2674fdc4aef7ae9ad2158a59994
Hemit Patel <hemitvpatel@gmail.com>
2025-04-22 13:53:56 -0400

    the basic click code rdy

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+import click
 
+@click.group()
+@click.option('--dbURL', prompt='AstraDB URL:',
+              help='The DB url to make changes on.')
+
+def main():
+  click.echo(click.style("Welcome to Talem AI CLI!"), fg="blue")
+
+if (__name__ == '__main__'):
+  main()
