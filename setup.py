2cba1cb387b355e0caf6dcb3b3ab3d511ed6619b
Hemit Patel <hemitvpatel@gmail.com>
2025-05-04 21:13:36 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='talemai',
-    version='0.1.6',
+    version='0.1.7',
     author='Hemit Patel',
     description='A description of your project',
     packages=find_packages(),
