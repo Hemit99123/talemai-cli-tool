09ae1434abd678302fee8f894a9398ec269aaad1
Hemit Patel <hemitvpatel@gmail.com>
2025-05-06 15:56:14 -0400

    updated verison

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='talem-ai-cli',
-    version='0.1.0',
+    version='0.0.0',
     author='Hemit Patel',
     description='Administrative tool for RAG apps',
     packages=find_packages(),
